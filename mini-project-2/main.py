182ea36f0501e83fd2a1a44aa82157b782ceb932
Mouncif00 <mouncif.belrhrib@final.edu.tr>
2026-03-26 21:03:10 +0200

    mini-project-2: main setup

---
+++ mini-project-2/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from database.connection import Settings
+from routes.events import event_router
+from routes.users import user_router
+
+app = FastAPI()
+
+app.include_router(event_router, prefix="/event")
+app.include_router(user_router, prefix="/user")
+
+
+@app.on_event("startup")
+async def start_db():
+    settings = Settings()
+    await settings.initialize_database()
