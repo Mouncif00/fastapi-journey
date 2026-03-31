14f683025464d1c8aedd8fc211bca1d40e27eb66
Mouncif00 <mouncif.belrhrib@final.edu.tr>
2026-03-31 17:39:19 +0300

    mini-project-2: users routes

---
+++ mini-project-2/routes/users.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException, status
+from models.users import User, UserSignIn
+from database.connection import Database
+
+user_router = APIRouter()
+user_db = Database(User)
+
+
+@user_router.post("/signup")
+async def signup(user: User):
+    existing = await User.find_one(User.email == user.email)
+    if existing:
+        raise HTTPException(status_code=409, detail="User exists")
+
+    await user_db.save(user)
+    return {"message": "User created"}
+
+
+@user_router.post("/signin")
+async def signin(user: UserSignIn):
+    existing = await User.find_one(User.email == user.email)
+
+    if not existing:
+        raise HTTPException(status_code=404)
+
+    if existing.password != user.password:
+        raise HTTPException(status_code=401)
+
+    return {"message": "Signed in"}
